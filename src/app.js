c29406b1a1e0496fb6fefbb76ad03154d058143e
PiyushBhandari06 <bhandaripiyush2003@gmail.com>
2025-08-06 14:31:32 +0530

    add setup app.js for all imports of routes,controller

--- src/app.js
+++ src/app.js
@@ -36,6 +36,14 @@ app.use(cookieParser())
 //🔘routes importing:-
 import userRouter from "./routes/user.routes.js"        
     //🔴Imp point-> you can define import name acc to your wish(userRouter here), but only when you have exported the file in default export
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 
 //🔘routes declaration:-
@@ -49,8 +57,15 @@ import userRouter from "./routes/user.routes.js"
     //An industry standard practice is: 
     //whenever you are defining your api, define its url such that it consists : 'you are defining api using - api' & 'the version of your api - v1/v2..'
 app.use("/api/v1/users", userRouter)
-    //working -> //http://localhost:8000/api/v1users
-
+    //working -> //http://localhost:8000/api/v1/users
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 // export default app;
 export { app }
