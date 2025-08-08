63c31b0921004f03da7d1dca2accef1dbe83eb12
PiyushBhandari06 <bhandaripiyush2003@gmail.com>
2025-08-08 17:54:59 +0530

    add complete healthcheck controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,13 +1,12 @@
-import {apiError} from "../utils/apiError.js"
 import {apiResponse} from "../utils/apiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
-
+//build a healthcheck response that simply returns the OK status as json with a message
 const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    return res
+        .status(200)
+        .json(new apiResponse(200, { message: "Everything is O.K" }, "Ok"));
 })
 
-export {
-    healthcheck
-    }
+export {healthcheck}
     
