2d9ebb4c69f477717cc6fa3c732a9673c3fe5544
PiyushBhandari06 <bhandaripiyush2003@gmail.com>
2025-08-16 17:09:54 +0530

    removed comments from dashboard.controller.js

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -6,33 +6,30 @@ import {apiError} from "../utils/apiError.js"
 import {apiResponse} from "../utils/apiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
-//Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 const getChannelStats = asyncHandler(async (req, res) => {
     const userId = req.user?._id;
 
-    const totalSubscribers = await subscription.aggregate([             //Runs an aggregation query on the subscription collection, to count total subscribers for this channel.
+    const totalSubscribers = await subscription.aggregate([             
         {
             $match: {               
                 channel: new mongoose.Types.ObjectId(userId)
-                //filters documents where channel equals the current user’s ObjectId (channel owner).
+
             }
         },
         {
-            $group: {                       //groups all matched documents together
-                _id: null,                  //(_id: null) don't assign any id
-                subscribersCount: {         //sums up the count ($sum: 1) to get total subscribers.
+            $group: {                       
+                _id: null,                  
+                subscribersCount: {         
                     $sum: 1 
                 }
             }
         }
     ]);
-    //totalSubscribers will be an array with one object like { _id: null, subscribersCount: <number> }.
 
-
-    const video = await Video.aggregate([                               //Runs an aggregation on the Video collection to calculate total likes, views, and videos for the channel owner.
+    const video = await Video.aggregate([                               
         {
             $match: {
-                owner: new mongoose.Types.ObjectId(userId)              //filters videos where owner is the current user.
+                owner: new mongoose.Types.ObjectId(userId)              
             }
         },
         {
@@ -41,20 +38,20 @@ const getChannelStats = asyncHandler(async (req, res) => {
                 localField: "_id",
                 foreignField: "video",
                 as: "likes"
-                //joins the likes collection, where video._id matches likes.video. This pulls all likes for each video into an array likes.
+
             }
         },
         {
-            $project: {             //why need this ? Before grouping, you want to shape each video document to include the specific fields you'll sum:
+            $project: {             
                 totalLikes: {
-                    $size: "$likes"         //takes the likes field just created(as: "likes",)from the joined document above
+                    $size: "$likes"         
                 },
-                totalViews: "$views",       //takes the views field from the video document.
-                totalVideos: 1              //sets to 1 for counting videos later.
+                totalViews: "$views",       
+                totalVideos: 1              
             }
         },
         {
-            $group: {                       //groups all videos into one group
+            $group: {                       
                 _id: null,
                 totalLikes: {
                     $sum: "$totalLikes"
@@ -63,13 +60,13 @@ const getChannelStats = asyncHandler(async (req, res) => {
                     $sum: "$totalViews"
                 },
                 totalVideos: {
-                    $sum: 1                 //total count of videos. //For each document in this group, add 1 to the sum.
+                    $sum: 1                 
                 }
             }
         }
     ]);
 
-    const channelStats = {      //Constructs a stats object to return.
+    const channelStats = {      
         totalSubscribers: totalSubscribers[0]?.subscribersCount || 0,
         totalLikes: video[0]?.totalLikes || 0,
         totalViews: video[0]?.totalViews || 0,
@@ -83,39 +80,36 @@ const getChannelStats = asyncHandler(async (req, res) => {
         );
 })
 
-
-
-//Get all the videos uploaded by the channel
 const getChannelVideos = asyncHandler(async (req, res) => {     
     const userId = req.user?._id;
 
-    const videos = await Video.aggregate([                          //Starts an aggregation pipeline on the Video collection, to fetch and transform video data.
+    const videos = await Video.aggregate([                          
         {
             $match: {
                 owner: new mongoose.Types.ObjectId(userId)
-                //Filters videos where the owner field matches the current user’s ObjectId.
+
             }
         },
         {
-            $lookup: {                          //Performs a left outer join with the likes collection.
+            $lookup: {                          
                 from: "likes",
-                localField: "_id",              //_id(object id) of each document in Video model/Collection
-                foreignField: "video",          //video field in likes Model
-                as: "likes"                     //Adds an array field likes to each video document containing all related like documents.
+                localField: "_id",              
+                foreignField: "video",          
+                as: "likes"                     
             }
         },
         {
             $addFields: {
-                createdAt: {                //Breaks down the original createdAt date into parts (year, month, day, etc.) using $dateToParts.
+                createdAt: {                
                     $dateToParts: { date: "$createdAt" }
                 },
-                likesCount: {               //Counts the number of likes for the video by taking the size of the likes array.
+                likesCount: {               
                     $size: "$likes"
                 }
             }
         },
         {
-            $sort: {                //Sorts the videos in descending order by createdAt.
+            $sort: {                
                 createdAt: -1
             }
         },
@@ -143,8 +137,6 @@ const getChannelVideos = asyncHandler(async (req, res) => {
         new apiResponse(200, videos, "channel stats fetched successfully")
     );
 })
-
-
 
 export {
     getChannelStats, 
