90fb162532541da472b53cb17b30a31dbe6ee75d
PiyushBhandari06 <bhandaripiyush2003@gmail.com>
2025-08-16 17:39:51 +0530

    removed comments from app.js & index.js

--- src/app.js
+++ src/app.js
@@ -4,38 +4,22 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-//app.use syntax will always be used for configuration & middlewares 
-// app.use(cors())         //majorly only this much is needed 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-    // This tells the browser and the server that it's okay to include credentials like:
-    //      Cookies
-    //      Authorization headers (e.g., Bearer tokens)
-    //      TLS client certificates
-    // in cross-origin HTTP requests.
+
 }))
 
-//this handles data coming thru form submission in frontend :
 app.use(express.json({limit: "16kb"}))          
-    // When a frontend (HTML/React/Angular, etc.) form submits data to the backend — typically using:
-    // fetch(), axios.post(), or <form method="POST">
-    // ...the form data is sent as part of the HTTP request body.
 
-//this handles data coming thru url :
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 
-//this handles data coming in file/folder format(pdf,images)
-app.use(express.static("public"))   //public is just the name for the public asset we have created in this project
+app.use(express.static("public"))   
 
-//this is used to access user's browser cookies and set them thru our server. so that we can perform CRUD opertaions on it
 app.use(cookieParser())
-//there are ways to keep secured cookies in user's browser that can only be read & remove by our server
 
-
-//🔘routes importing:-
 import userRouter from "./routes/user.routes.js"        
-    //🔴Imp point-> you can define import name acc to your wish(userRouter here), but only when you have exported the file in default export
+
 import healthcheckRouter from "./routes/healthcheck.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
@@ -45,19 +29,8 @@ import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
 
-
-//🔘routes declaration:-
-// app.get()       //we can't use this here
-                   //we are only able to use this syntax when we are defining the routes and controller in it, but now we have defined route in a separate file.
-
-// app.use("/users", userRouter)          //now to get router, we will need to use middleware syntax[that is use()] 
-    //working -> //http://localhost:8000/users -> this gives controll to userRouter method defined in user.routes.js file
-    
-
-    //An industry standard practice is: 
-    //whenever you are defining your api, define its url such that it consists : 'you are defining api using - api' & 'the version of your api - v1/v2..'
 app.use("/api/v1/users", userRouter)
-    //working -> //http://localhost:8000/api/v1/users
+
 app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
@@ -67,5 +40,4 @@ app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
 
-// export default app;
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-// As early as possible in your application, import and configure dotenv:
-// we want -> as soon as our app loads, our env variables should be available everywhere, we often load our env variables in the first file thats being loaded(index.js here)
+import dotenv from "dotenv";                            
 
-// require('dotenv').config({path:'./env'})             //-> CommonJS syntax
-    //this method creates inconsistency in our project code, since everything else is in ES6 syntax
-import dotenv from "dotenv";                            //-> ES6 syntax 
-    //this method creates consistency, 
-    // but it requires an addition of experimental feature in package.json file
-    // explanation -> https://youtu.be/w4z8Py-UoNk?si=OcD_NEJZH7r9uw3I , from 32:33 to
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
@@ -14,8 +7,7 @@ dotenv.config({
     path:'./.env'
 })
 
-// METHOD : 1
-connectDB() // since async functin returns a promise
+connectDB() 
 .then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
         console.log(`Server is running at PORT: ${process.env.PORT}`);
@@ -24,37 +16,3 @@ connectDB() // since async functin returns a promise
 .catch((error)=>{
     console.log("MONGO-DB connection failed !!! ", error);
 })
-
-
-
-/*  
-//METHOD : 2
-
-    //approach 2a:-                         // create a function and call it
-
-// function connectDB(){}       
-// connectDB()
-
-
-    //approach 2b:-     // ()() - IIFE    // create a function(arrow fn here) and it will be immediately invoked using IIFE
-  
-import express from "express";
-const app = express();
-;(async ()=>{           //use semicolon(;) infront of IIFE so that incase if your editor didn't apply semi-colon on the line before iife, a prblm can be occur.
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-
-        app.on("error", (error)=> {
-            console.log("ERROR: ",error);
-            throw error
-        })
-        app.listen(process.env.PORT, ()=>{
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw error                                 //process exits when we use-> throw error
-    }
-})()    
-
-*/
