3bd05a322734ca3a9a1d202c641318a13056520e
PiyushBhandari06 <bhandaripiyush2003@gmail.com>
2025-08-16 17:25:53 +0530

    removed comments from healthcheck.controller.js

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,7 +1,6 @@
 import {apiResponse} from "../utils/apiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
-//build a healthcheck response that simply returns the OK status as json with a message
 const healthcheck = asyncHandler(async (req, res) => {
     return res
         .status(200)
@@ -9,4 +8,3 @@ const healthcheck = asyncHandler(async (req, res) => {
 })
 
 export {healthcheck}
-    
